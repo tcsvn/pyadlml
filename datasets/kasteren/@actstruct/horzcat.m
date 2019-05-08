c3ae6bcb025afb51a5bf900554fb9256bd9e09de
Christian Meier <account@meier-lossburg.de>
2019-05-08 08:11:54 +0000

    Deleted dataset due to cloning time and volume being to high

--- datasets/kasteren/@actstruct/horzcat.m
+++
@@ -1,5 +0,0 @@
-function [as2] = horzcat(as, varargin)
-
-
-error('Cannot concatenate actstruct horizontaly, try as = [as1 ; as2] instead')
-
--- datasets/kasteren/sensorGUI/lineClickCallback.m
+++
@@ -1,8 +0,0 @@
-function lineClickCallback(src, evt)
-
-% Read label from clicked item 
-label = get(src, 'tag');
-
-szOut = ['Activity selected: ' label];
-hUI = findobj('tag', 'unique_static_output');
-set(hUI, 'String', label);
