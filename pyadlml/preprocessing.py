39045979ee1831afc865e9811089caa590647958
Christian Meier <account@meier-lossburg.com>
2020-09-03 21:03:55 +0200

    added lagged cp, lagged lf reps. added universal lagged labler

--- pyadlml/preprocessing.py
+++ pyadlml/preprocessing.py
@@ -1,8 +1,10 @@
 from pyadlml.dataset.representations.raw import create_raw 
 from pyadlml.dataset.representations.changepoint import create_changepoint
 from pyadlml.dataset.representations.lastfired import create_lastfired
-from pyadlml.dataset.representations.image import create_lagged_raw
+from pyadlml.dataset.representations.image import create_lagged_raw, create_lagged_lastfired, \
+                                            create_lagged_changepoint
 from pyadlml.dataset._dataset import label_data
+from pyadlml.dataset.devices import device_rep1_2_rep3
 import sklearn.preprocessing as preprocessing
 import pandas as pd
 import numpy as np
@@ -193,30 +195,46 @@ class LaggedRawEncoder():
             t_res=self.t_res, 
             sample_strat=self.sample_strat)
 
-    
-class LaggedRawLabelEncoder():
+class LaggedLabelEncoder():
     """
     wrapper around labelencoder to handle time series data
     """
-    def __init__(self, df_devices, window_size, t_res=None, sample_strat='ffill', idle=False):
-        self.labels = None
+    def __init__(self, df_devices, window_size, t_res=None, idle=False):
         self.window_size = window_size
         self.t_res = t_res
-        self.raw = create_raw(df_devices, t_res=t_res, sample_strat=sample_strat)
         self.idle = idle
         self._lbl_enc = preprocessing.LabelEncoder()
+        self.df_devices = df_devices
+        self.df_index = self._create_index(df_devices, t_res)
+        
+    def _create_index(self, df_devices, t_res):
+        """
+        create the dummy dataframe for the index from the devices
+        index | val
+        """
+        df = device_rep1_2_rep3(df_devices.copy())
+        df = df.pivot(index='time', columns='device', values='val').iloc[:,:1]
+        df = df.astype(bool) # just to have a lower memory footprint
+        
+        # resample with frequency
+        resampler = df.resample(t_res, kind='timestamp')
+        df_index = resampler.sum()
+        df_index.columns = ['val']
+        df_index['val'] = 1
+        return df_index
 
+        
     def fit(self, df_activities):
-        df = label_data(self.raw, df_activities, self.idle)
+        df = label_data(self.df_index, df_activities, self.idle)
         # start where the labeling begins
         df = df.iloc[self.window_size:,:]
         self._lbl_enc.fit(df['activity'].values)
 
     def fit_transform(self, df_activities):
-        df = label_data(self.raw, df_activities, self.idle)
+        df = label_data(self.df_index, df_activities, self.idle)
         df = df.iloc[self.window_size:,:]
         encoded_labels = self._lbl_enc.fit_transform(df['activity'].values)
-        return encoded_labels
+        return pd.DataFrame(index=df.index, data=encoded_labels, columns=['activity'])
 
     def inverse_transform(self, x, retain_index=False):
         """
@@ -228,7 +246,7 @@ class LaggedRawLabelEncoder():
         if isinstance(x, np.ndarray):
             res = self._lbl_enc.inverse_transform(x)
             if retain_index:
-                return pd.Series(data=res, index=self.df_devices.index[:len(res)])
+                return pd.Series(data=res, index=self.df_index.index[:len(res)])
             else:
                 return res
                 
@@ -239,17 +257,84 @@ class LaggedRawLabelEncoder():
         else:
             raise ValueError
 
-    def set_params(self, t_res=None, sample_strat=None):
+    def set_params(self, t_res=None, idle=None, window_size=None):
         if t_res is not None:
             self.t_res = t_res
-        if sample_strat is not None:
-            self.sample_strat = sample_strat
+            self.df_index = self._create_index(self.df_devices, t_res)
+        if idle is not None:
+            self.idle = idle
+        if window_size is not None:
+            self.window_size = window_size
 
     def get_params(self):
-        return self.labels, self.idle, self.raw
+        return {'window_size': self.window_size, 'idle': self.idle, 't_res':self.t_res}
 
     def transform(self, df_activities):
-        df = label_data(self.raw, df_activities, self.idle)
+        df = label_data(self.df_index, df_activities, self.idle)
         df = df.iloc[self.window_size:,:]
         encoded_labels = self._lbl_enc.transform(df['activity'].values)
-        return encoded_labels
+        return pd.DataFrame(index=df.index, data=encoded_labels, columns=['activity'])
+
+class LaggedChangepointEncoder():
+    def __init__(self, window_size, t_res=None):
+        self.lgd_cp = None
+        self.t_res = t_res
+        self.window_size = window_size
+
+    def fit(self, df_devices):
+        self.lgd_cp = create_lagged_changepoint(
+            df_devices, 
+            window_size=self.window_size, 
+            t_res=self.t_res)
+
+    def fit_transform(self, df_devices):
+        self.fit(df_devices)
+        return self.lgd_cp
+
+    def inverse_transform(self, lgd_raw):
+        """
+        """
+        raise NotImplementedError
+
+    def set_params(self, t_res=None, window_size=10):
+        if t_res is not None:
+            self.t_res = t_res
+        raise NotImplementedError
+
+    def transform(self, df_devices):
+        return create_lagged_changepoint(
+            df_devices, 
+            window_size=self.window_size, 
+            t_res=self.t_res)
+
+class LaggedLastFiredEncoder():
+    def __init__(self, window_size, t_res=None):
+        self.lgd_lf = None
+        self.t_res = t_res
+        self.window_size = window_size
+
+    def fit(self, df_devices):
+        self.lgd_lf = create_lagged_lastfired(
+            df_devices, 
+            window_size=self.window_size, 
+            t_res=self.t_res)
+
+    def fit_transform(self, df_devices):
+        self.fit(df_devices)
+        return self.lgd_lf
+
+    def inverse_transform(self, lgd_lf):
+        """
+        """
+        raise NotImplementedError
+
+    def set_params(self, t_res=None, window_size=10):
+        if t_res is not None:
+            self.t_res = t_res
+        raise NotImplementedError
+
+    def transform(self, df_devices):
+        return create_lagged_lastfired(
+            df_devices, 
+            window_size=self.window_size, 
+            t_res=self.t_res)
