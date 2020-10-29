6a6d4227082605bc066a033baf9fac8135ec2550
Christian Meier <account@meier-lossburg.com>
2020-10-29 12:21:24 +0100

    cleanup and fixed imports

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md', 'r') as history_file:
 
 setup_args = dict(
   name = "pyadlml",
-  version = "0.0.1-alpha",
+  version = "0.0.2-alpha",
   url = "https://github.com/tcsvn/pyadlml",
   author = "Christian Meier",
   description = "Sklearn like library supporting numerous Activity of Daily Livings datasets",
