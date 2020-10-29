50550385fb2f4d3f4eb9a6b797ef10f7a6985bf0
Christian Meier <account@meier-lossburg.com>
2020-10-29 10:57:46 +0100

    ready for pipy publish

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 from setuptools import setup, find_packages
 
-setup(
-  name = "hassbrain_algorithm",
-  version = "0.0.5",
-  url = "https://gitlab.com/hassbrain/hassbrain_algorithm",
+with open('README.md', 'r') as readme_file:
+  README = readme_file.read()
+
+with open('HISTORY.md', 'r') as history_file:
+  HISTORY = history_file.read()
+
+setup_args = dict(
+  name = "pyadlml",
+  version = "0.0.1-alpha",
+  url = "https://github.com/tcsvn/pyadlml",
   author = "Christian Meier",
-  author_email = "christian@meier-lossburg.de",
+  description = "Sklearn like library supporting numerous Activity of Daily Livings datasets",
+  long_description_content_type="text/markdown",
+  long_description=README + '\n\n' + HISTORY,
+  author_email = "account@meier-lossburg.de",
   license = "MIT",
   packages = find_packages(),
-  install_requires = ['numpy']
+  keywords = ['Activity of Daily Living'],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "Topic :: Scientific/Engineering :: Visualization"
+  ],
 )
+
+install_requires = ['numpy', 'pandas']
+
+if __name__ == '__main__':
+  setup(**setup_args, install_requires=install_requires)
