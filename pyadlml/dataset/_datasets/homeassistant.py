ab459288b38dced596d82632bec17a5285a8e62c
Christian Meier <account@meier-lossburg.com>
2020-12-11 12:02:44 +0100

    added functionality for start end end time

--- pyadlml/dataset/_datasets/homeassistant.py
+++ pyadlml/dataset/_datasets/homeassistant.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pyadlml.dataset import TIME, DEVICE, VAL
+import datetime
 
-def load_homeassistant(db_url, limit=5000000, start_time=None):
+def load_homeassistant(db_url, limit=5000000, start_time=None, end_time=None):
     """ returns a dataframe representation of the homeassistant database
     Parameters
     ----------
@@ -15,7 +16,7 @@ def load_homeassistant(db_url, limit=5000000, start_time=None):
     -------
     df : pd.DataFrame
     """
-    if start_time == None:
+    if start_time == None and end_time == None:
         query = f"""
         SELECT entity_id, state, last_changed
         FROM states
@@ -25,20 +26,22 @@ def load_homeassistant(db_url, limit=5000000, start_time=None):
         LIMIT {limit}
         """
     else:
+        if end_time == None:
+            end_time = datetime.datetime.now()
         query = f"""
         SELECT entity_id, state, last_changed
         FROM states
         WHERE
             state NOT IN ('unknown', 'unavailable') AND
             last_changed BETWEEN 
-            '{start_time}' AND date('now')
+            '{start_time}' AND '{end_time}'
         ORDER BY last_changed ASC
         LIMIT {limit}        
         """
     df = pd.read_sql_query(query, db_url)
     return df
 
-def load_homeassistant_devices(db_url, device_list, start_time=None):
+def load_homeassistant_devices(db_url, device_list, start_time=None, end_time=None):
     """ creates as dataframe in representation 1 from homeassistant database
     
     Parameters
@@ -49,12 +52,14 @@ def load_homeassistant_devices(db_url, device_list, start_time=None):
         device selection to filter
     start_time : pd.Timestamp
         the start time from when to filter
+    end_time : pd.Timestamp
+        the end time from when to filter
     Returns
     -------
     df : pd.DataFraem
     """
     
-    df = load_homeassistant(db_url, start_time=start_time)
+    df = load_homeassistant(db_url, start_time=start_time, end_time=end_time)
     df = df[df['entity_id'].isin(device_list)]
     df[TIME] = pd.to_datetime(df['last_changed'])
     df[VAL] = (df['state'] == 'on').astype(int)
