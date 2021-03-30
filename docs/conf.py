3954cda8dee191f5693e2de18260e548dd1a6e36
Christian Meier <account@meier-lossburg.com>
2021-03-30 23:01:40 +0200

    additional stuff

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "pydata_sphinx_theme"
-html_logo = "_static/logo.png"
+html_logo = "_static/logo.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
